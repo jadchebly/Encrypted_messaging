e5a974b757455c45ed22a4a6d2c36bdaf217ae44
jadchebly <159762508+jadchebly@users.noreply.github.com>
2024-11-30 12:42:52 +0100

    Update client.c

--- client.c
+++ client.c
@@ -3,78 +3,62 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 
-#define SERVER_IP "127.0.0.1"
-#define SERVER_PORT 12345
+#define PORT 12345
 #define BUFFER_SIZE 1024
-#define DELIMITER "\n" // To mark the end of each message
 
-void start_client() {
+
+
+void *receive_messages(void *arg) {
+    int socket = *((int *)arg);
+    char buffer[BUFFER_SIZE];
+    while (1) {
+        int bytes_received = recv(socket, buffer, sizeof(buffer), 0);
+        if (bytes_received <= 0) {
+            printf("Disconnected from server\n");
+            exit(1);
+        }
+        buffer[bytes_received] = '\0';
+        printf("%s", buffer);
+    }
+    return NULL;
+}
+
+int main() {
     int client_socket;
     struct sockaddr_in server_addr;
     char buffer[BUFFER_SIZE];
 
-    // Create the socket
     client_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (client_socket < 0) {
-        perror("[CLIENT ERROR] Socket creation failed");
-        exit(EXIT_FAILURE);
+    if (client_socket == -1) {
+        perror("Socket creation failed");
+        exit(1);
     }
 
-    // Configure server address
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(SERVER_PORT);
-    inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr);
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = inet_addr("192.168.1.59");
 
-    // Connect to the server
-    if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-        perror("[CLIENT ERROR] Connection to server failed");
-        close(client_socket);
-        exit(EXIT_FAILURE);
+    if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Connection failed");
+        exit(1);
     }
 
-    printf("[CLIENT] Connected to server at %s:%d\n", SERVER_IP, SERVER_PORT);
+    char username[50];
+    printf("Enter your username: ");
+    fgets(username, sizeof(username), stdin);
+    username[strcspn(username, "\n")] = '\0';
+    send(client_socket, username, strlen(username), 0);
 
-    // Communication loop
+    pthread_t thread;
+    pthread_create(&thread, NULL, receive_messages, &client_socket);
+
     while (1) {
-        printf("[CLIENT] Enter a message (type 'exit' to quit): ");
-        fgets(buffer, BUFFER_SIZE, stdin);
-
-        // Remove newline character from input
-        size_t len = strlen(buffer);
-        if (len > 0 && buffer[len - 1] == '\n') {
-            buffer[len - 1] = '\0';
-        }
-
-        // Check for exit condition
-        if (strcmp(buffer, "exit") == 0) {
-            printf("[CLIENT] Disconnecting.\n");
-            break;
-        }
-
-        // Send message to server
-        strcat(buffer, DELIMITER); // Add delimiter to mark message end
+        fgets(buffer, sizeof(buffer), stdin);
         send(client_socket, buffer, strlen(buffer), 0);
-
-        // Receive server response
-        memset(buffer, 0, BUFFER_SIZE);
-        int bytes_received = recv(client_socket, buffer, BUFFER_SIZE - 1, 0);
-        if (bytes_received <= 0) {
-            printf("[CLIENT] Server disconnected.\n");
-            break;
-        }
-
-        // Ensure buffer ends with a null terminator
-        buffer[bytes_received] = '\0';
-        printf("[CLIENT RECEIVED] %s", buffer);
     }
 
-    // Close socket
     close(client_socket);
-    printf("[CLIENT] Connection closed.\n");
-}
-
-int main() {
-    start_client();
     return 0;
 }
